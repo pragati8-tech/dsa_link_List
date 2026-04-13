dd30c9dc41966ba62863f742f56747718450ba65
pragatiagrawal69 <your.email@example.com>
2026-04-14 00:03:47 +0530

    fibonacci and power code added

--- linearSort.js
+++ linearSort.js
@@ -1,4 +1,44 @@
-let nums=[2,7,11,15]
+
+
+function fibonacci(n){
+    if(n==0){
+        return 0
+    }
+    if(n==1){
+        return 1
+    }
+    return fibonacci(n-1) + fibonacci(n-1)
+}
+console.log(fibonacci(10))
+// $ node Fi-bo-na-c-ci.js 
+// 512
+
+// function factorial(n){
+//     if(n==1|| n==0){
+//         return 1
+//     }
+//     return n*factorial(n-1)
+// }
+// console.log(factorial(5)) 
+
+
+
+// function powerr(base,power){
+//     if(power==1){
+//         return 1
+//     }
+// let ans = powerr(base, Math.floor(power/2))
+// if(!power%2 ===0){
+//     return base * ans * ans
+// } else{
+//     return ans * ans
+// }
+// }
+// console.log(powerr(2,10))
+
+// // $ node power.js
+// // 128
+// let nums=[2,7,11,15]
 
 // function twoSum(nums,target){
 //     for(let i=0;i<nums.length;i++){
